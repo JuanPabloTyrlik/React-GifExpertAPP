ba2308401e203421bdd09a42ea536ea0f4a77d5a
Juan Pablo Tyrlik <juan.pablo.tyrlik@gmail.com>
2020-07-29 19:07:17 -0300

    Manifest y Docs (Build)

---
+++ docs/precache-manifest.7bea22bb0bacb566fe9464d9e6cecda3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee3e3e1af4e0237dcecb6b319b3b1666",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9f1daa0fd394a7b9219",
+    "url": "/static/css/main.9b28869a.chunk.css"
+  },
+  {
+    "revision": "ccd9761b7db298367323",
+    "url": "/static/js/2.dab6626b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dab6626b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9f1daa0fd394a7b9219",
+    "url": "/static/js/main.19755a95.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
